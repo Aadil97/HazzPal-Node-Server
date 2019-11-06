df8efee52e5e8845b6ecba01e6a4bd7cca39e699
Aadil97 <aadil97@hotmail.co.uk>
2019-11-06 12:53:46 +0000

    Added mongoDB

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,22 @@ var results = require('./results.json');
 var endData;
 var numbers = require('./numbers.json');
 
-//Mainpage to test if it works0
+//Mongodb
+const MongoClient = require('mongodb').MongoClient;
+
+// replace the uri string with your connection string.
+const uri = config.mongoDBconnect
+MongoClient.connect(uri, function(err, client) {
+   if(err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+   }
+   console.log('Connected...');
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
+});
+
+//Mainpage to test if it works
 router.get('/', function(req,res,next){
   res.render('index', {})
 
